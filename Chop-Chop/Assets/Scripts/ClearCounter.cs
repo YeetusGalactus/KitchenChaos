a11ef7cb7e26c66e6a038bb4a63059f3b4a76eae
DevadharsanVS <224190116+YeetusGalactus@users.noreply.github.com>
2026-01-01 09:19:38 -0800

    Added interactable kitchen objects
    Created tomato and cheese block prefabs, modified them into scriptable objects. Modified scripts to allow the player to spawn the kitchen objects on top of clear counters

--- Chop-Chop/Assets/Scripts/ClearCounter.cs
+++ Chop-Chop/Assets/Scripts/ClearCounter.cs
@@ -2,7 +2,14 @@ using UnityEngine;
 
 public class ClearCounter : MonoBehaviour
 {
+    [SerializeField] private KitchenObjectSO kitchenObjectSO;
+    [SerializeField] private Transform CounterTopPoint;
+
     public void Interact() {
         Debug.Log("Interacted");
+        Transform kitchenObjectTransform = Instantiate(kitchenObjectSO.prefab, CounterTopPoint);
+        kitchenObjectTransform.localPosition = Vector3.zero;
+
+        Debug.Log(kitchenObjectTransform.GetComponent<KitchenObject>().GetKitchenObjectSO().objectName);
     }
 }
